9548389213b74f7eed1ec7057c0802571e77c25b
Benjamin Nicholls <nicholls.ben@gmail.com>
2023-03-14 14:25:56 -0400

    genericalize some math functions, update deps

--- go.mod
+++ go.mod
@@ -1,5 +1,8 @@
 module github.com/bennicholls/tyumi
 
-go 1.16
+go 1.20
 
-require github.com/veandco/go-sdl2 v0.4.5
+require (
+	github.com/veandco/go-sdl2 v0.4.5
+	golang.org/x/exp v0.0.0-20230310171629-522b1b587ee0
+)
--- util/math.go
+++ util/math.go
@@ -1,11 +1,15 @@
-//utility functions for math. the Go standard library has some of these as well, but in a lot of situations they are too
-//generic and weighed down by unnecessary checks and the like. the functions here are expected to be run in tight loops,
-//so we strip out unneeded cruft. other things are just useful functions that I made up as I needed them.
+// utility functions for math. the Go standard library has some of these as well, but in a lot of situations they are too
+// generic and weighed down by unnecessary checks and the like. the functions here are expected to be run in tight loops,
+// so we strip out unneeded cruft. other things are just useful functions that I made up as I needed them.
 package util
 
-import "math"
+import (
+	"math"
 
-//Pow is an integer power function. Doesn't ~~do~~ negative exponents. Totally does 0 though.
+	"golang.org/x/exp/constraints"
+)
+
+// Pow is an integer power function. Doesn't ~~do~~ negative exponents. Totally does 0 though.
 func Pow(val, exp int) int {
 	v := 1
 	for i := 0; i < exp; i++ {
@@ -14,46 +18,36 @@ func Pow(val, exp int) int {
 	return v
 }
 
-//Abs returns the absolute value of val
-func Abs(val int) int {
+// Abs returns the absolute value of val
+func Abs[T constraints.Signed](val T) T {
 	if val < 0 {
 		return val * (-1)
 	}
 	return val
 }
 
-//Max returns the max of two integers. Duh.
-func Max(i, j int) int {
+// Max returns the max of two numbers. Duh.
+func Max[T constraints.Ordered](i, j T) T {
 	if i < j {
 		return j
 	}
 	return i
 }
 
-func MaxF(i, j float64) float64 {
-	if i < j {
-		return j
-	}
-	return i
-}
-
-//Min is the opposite of max.
-func Min(i, j int) int {
+// Min is the opposite of max.
+func Min[T constraints.Ordered](i, j T) T {
 	if i > j {
 		return j
 	}
 	return i
 }
 
-func MinF(i, j float64) float64 {
-	if i > j {
-		return j
+// Clamp checks if min <= val <= max. If val < min, returns min. If val > max, returns max. Otherwise returns val.
+func Clamp[T constraints.Ordered](val, min, max T) T {
+	if min > max {
+		min, max = max, min
 	}
-	return i
-}
 
-//Clamp checks if min <= val <= max. If val < min, returns min. If val > max, returns max. Otherwise returns val.
-func Clamp(val, min, max int) int {
 	if val <= min {
 		return min
 	} else if val >= max {
@@ -62,12 +56,13 @@ func Clamp(val, min, max int) int {
 	return val
 }
 
-//ModularClamp is like clamp but instead of clamping at the endpoints, it overflows/underflows back to the other side of
-//the range. The second return param is the number of overflow cycles. negative for underflow, 0 for none, positive for 
-//overflow. This kind of function probably has an actual name but hell if I know what it is.
+// ModularClamp is like clamp but instead of clamping at the endpoints, it overflows/underflows back to the other side of
+// the range. The second return param is the number of overflow cycles. negative for underflow, 0 for none, positive for
+// overflow. This kind of function probably has an actual name but hell if I know what it is.
+//NOTE: this function kind of doesn't work as expected since it is inclusive at the end points.
+//THINK: should this just be removed? it's pretty niche.
 func ModularClamp(val, min, max int) (int, int) {
 	if min > max {
-		//if someone foolishly puts their min higher than max, swap
 		min, max = max, min
 	}
 
@@ -83,20 +78,20 @@ func ModularClamp(val, min, max int) (int, int) {
 	return val, 0
 }
 
-//RoundFloatToInt rounds a float to an int in the way you'd expect. It's the way I expect anyways.
+// RoundFloatToInt rounds a float to an int in the way you'd expect. It's the way I expect anyways.
 func RoundFloatToInt(f float64) int {
 	return int(f + math.Copysign(0.5, f))
 }
 
-//Lerp linearly interpolates a range (min-max) over (steps) intervals, and returns the (val)th step. Currently does this
-//via a conversion to float64, so there might be some rounding errors in here I don't know about.
-func Lerp(min, max, val, steps int) int {
+// Lerp linearly interpolates a range (min-max) over (steps) intervals, and returns the (val)th step. Currently does this
+// via a conversion to float64, so there might be some rounding errors in here I don't know about.
+func Lerp[T constraints.Integer|constraints.Float](min, max T, val, steps int) T {
 	if val >= steps {
-		return max
+		return T(max)
 	} else if val <= 0 {
-		return min
+		return T(min)
 	}
 
 	stepVal := float64(max-min) / float64(steps)
-	return int(float64(min) + stepVal*float64(val))
+	return T(float64(min) + stepVal*float64(val))
 }

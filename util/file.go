3d9ac42d0d68583d2ae33dfdaa071540e11747fc
Benjamin Nicholls <nicholls.ben@gmail.com>
2024-11-29 16:09:55 -0500

    update from deprecated io function

--- util/file.go
+++ util/file.go
@@ -1,27 +1,27 @@
-//utility functions for dealing with files
+// utility functions for dealing with files
 package util
 
 import (
-	"io/ioutil"
+	"os"
 	"strings"
 )
 
-//GetFileList returns a list of all files in the provided directory. If ext is provided, it only includes files with
-//that extension.
+// GetFileList returns a list of all files in the provided directory. If ext is provided, it only includes files with
+// that extension.
 func GetFileList(dirPath, ext string) (files []string, err error) {
 	files = make([]string, 0)
 
-	dirContents, err := ioutil.ReadDir(dirPath)
+	dirContents, err := os.ReadDir(dirPath)
 	if err != nil {
 		return
 	}
 
-	for i, file := range dirContents {
-		if !file.IsDir() {
-			if ext != "" && !strings.HasSuffix(dirContents[i].Name(), ext) {
+	for _, entry := range dirContents {
+		if !entry.IsDir() {
+			if ext != "" && !strings.HasSuffix(entry.Name(), ext) {
 				continue
 			}
-			files = append(files, dirContents[i].Name())
+			files = append(files, entry.Name())
 		}
 	}
 

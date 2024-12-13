c692740120151fbf4651fbbb13afabf1eaef93ab
Benjamin Nicholls <nicholls.ben@gmail.com>
2024-12-13 15:39:02 -0500

    vec.Direction
    - can use Direction type for cardinal directions (for now).

--- vec/coord.go
+++ vec/coord.go
@@ -27,9 +27,56 @@ func (c1 Coord) Subtract(c2 Coord) Coord {
 	return Coord{c1.X - c2.X, c1.Y - c2.Y}
 }
 
+func (c Coord) Step(d Direction) Coord {
+	return c.Add(Coord(d))
+}
+
 //ManhattanDistance calculates the manhattan (or taxicab) distance on a square grid.
 func ManhattanDistance(c1, c2 Coord) int {
 	return util.Abs(c2.X-c1.X) + util.Abs(c2.Y-c1.Y)
+}
+
+
+type Direction Vec2i
+
+var (
+	DIR_NONE  Direction = Direction{0, 0}
+	DIR_UP    Direction = Direction{0, -1}
+	DIR_DOWN  Direction = Direction{0, 1}
+	DIR_LEFT  Direction = Direction{-1, 0}
+	DIR_RIGHT Direction = Direction{1, 0}
+)
+
+func (d Direction) Inverted() Direction {
+	return Direction{-d.X, -d.Y}
+}
+
+func (d Direction) RotateCW() Direction {
+	switch d {
+	case DIR_UP:
+		return DIR_RIGHT
+	case DIR_RIGHT:
+		return DIR_DOWN
+	case DIR_DOWN:
+		return DIR_LEFT
+	case DIR_LEFT:
+		return DIR_UP
+	}
+	return DIR_NONE
+}
+
+func (d Direction) RotateCCW() Direction {
+	switch d {
+	case DIR_UP:
+		return DIR_LEFT
+	case DIR_RIGHT:
+		return DIR_UP
+	case DIR_DOWN:
+		return DIR_RIGHT
+	case DIR_LEFT:
+		return DIR_DOWN
+	}
+	return DIR_NONE
 }
 
 //TODO this should be somewhere else...

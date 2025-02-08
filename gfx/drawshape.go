9fecf1263a9af550103b534591198592c3f07d1c
Benjamin Nicholls <nicholls.ben@gmail.com>
2025-02-08 11:28:39 -0500

    Box drawing!
    - can draw a nice box using the line-linking data we just moved

--- gfx/drawshape.go
+++ gfx/drawshape.go
@@ -1,6 +1,9 @@
 package gfx
 
-import "github.com/bennicholls/tyumi/vec"
+import (
+	"github.com/bennicholls/tyumi/gfx/col"
+	"github.com/bennicholls/tyumi/vec"
+)
 
 // Draws the provided visuals to the entire rectangular area. Optionally takes a 2nd Visuals that will be drawn
 // on the interior of the rect, making the normal visuals will be the border.
@@ -61,5 +64,44 @@ func (c *Canvas) DrawCircle(center vec.Coord, depth, r int, v Visuals, fill bool
 	}
 }
 
+// DrawBox draws a box on the edges of the provided area, respecting depth. line is the type of line to use: thin or
+// thick. Does not draw anything if the dimensions of the box aren't at least 2x2.
+func (c *Canvas) DrawBox(box vec.Rect, depth int, line LineType, colours col.Pair) {
+	// if not doing a linking line, just call the regular old rect drawing function
+	if line == LINETYPE_NONE {
+		c.DrawRect(box, depth, NewGlyphVisuals(GLYPH_BLOCK, colours))
+		return
+	}
+
+	//these boxes are too small to draw
+	if box.Area() == 0 || box.W == 1 || box.H == 1 {
+		return
+	}
+
+	style := LineStyles[line]
+	
+	//draw corners
+	corners := box.Corners()
+	c.DrawVisuals(corners[0], depth, NewGlyphVisuals(style.Glyphs[LINK_DR], colours))
+	c.DrawVisuals(corners[1], depth, NewGlyphVisuals(style.Glyphs[LINK_DL], colours))
+	c.DrawVisuals(corners[2], depth, NewGlyphVisuals(style.Glyphs[LINK_UL], colours))
+	c.DrawVisuals(corners[3], depth, NewGlyphVisuals(style.Glyphs[LINK_UR], colours))
+	
+	//draw sides
+	brush := NewGlyphVisuals(GLYPH_NONE, colours)
+	if box.W > 2 {
+		brush.Glyph = style.Glyphs[LINK_LR]
+		top := vec.Line{corners[0].Step(vec.DIR_RIGHT), corners[1].Step(vec.DIR_LEFT)}
+		bottom := vec.Line{corners[2].Step(vec.DIR_LEFT), corners[3].Step(vec.DIR_RIGHT)}
+		c.DrawLine(top, depth, brush)
+		c.DrawLine(bottom, depth, brush)
+	}
+
+	if box.H > 2 {
+		brush.Glyph = style.Glyphs[LINK_UD]
+		right := vec.Line{corners[1].Step(vec.DIR_DOWN), corners[2].Step(vec.DIR_UP)}
+		left := vec.Line{corners[3].Step(vec.DIR_UP), box.Coord.Step(vec.DIR_DOWN)}
+		c.DrawLine(left, depth, brush)
+		c.DrawLine(right, depth, brush)
 	}
 }

d729dd1909b6154c78a0823af77fdf7c7370c3fb
Benjamin Nicholls <nicholls.ben@gmail.com>
2025-03-30 14:58:13 -0400

    export a canvas to .xp file!
    - can now write a specific canvas to an .xp file. can be used for simple screenshots, or for debugging maddening UI issues
    - this depends on new functionality in reximage, so go.mod also got an update

---
+++ gfx/export.go
@@ -0,0 +1,34 @@
+package gfx
+
+import (
+	"github.com/bennicholls/reximage"
+	"github.com/bennicholls/tyumi/log"
+)
+
+// ExportToXP writes the contents of the canvas to an .xp file at the given path.
+func (c Canvas) ExportToXP(path string) {
+	var image reximage.ImageData
+	image.Init(c.size.W, c.size.H)
+
+	for i, cell := range c.cells {
+		cellData := reximage.CellData{}
+		cellData.SetColoursARGB(cell.Colours.Fore, cell.Colours.Back)
+		switch cell.Mode {
+		case DRAW_GLYPH:
+			cellData.Glyph = uint32(cell.Glyph)
+		case DRAW_TEXT:
+			cellData.Glyph = uint32(GLYPH_QUESTION)
+		case DRAW_NONE:
+			cellData.Clear()
+		}
+		err := image.SetCell(i%c.size.W, i/c.size.W, cellData)
+		if err != nil {
+			log.Error("Error setting cell during export: ", err)
+		}
+	}
+
+	err := reximage.Export(image, path)
+	if err != nil {
+		log.Error("Could not export image: ", err)
+	}
+}
--- go.mod
+++ go.mod
@@ -3,7 +3,7 @@ module github.com/bennicholls/tyumi
 go 1.24.0
 
 require (
-	github.com/bennicholls/reximage v1.0.0
+	github.com/bennicholls/reximage v1.1.0
 	github.com/veandco/go-sdl2 v0.4.40
-	golang.org/x/exp v0.0.0-20250228200357-dead58393ab7
+	golang.org/x/exp v0.0.0-20250305212735-054e65f0b394
 )

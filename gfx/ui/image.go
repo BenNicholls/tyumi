e2cadece7a791c7ad2ed8a5669aeb19fca048bdd
Benjamin Nicholls <nicholls.ben@gmail.com>
2025-04-10 11:36:44 -0400

    better xp image loading for image element
    - now can change image on the fly
    - can elect to set up the element without loading anything up front if you like

--- gfx/ui/image.go
+++ gfx/ui/image.go
@@ -21,11 +21,31 @@ func NewImage(pos vec.Coord, depth int, path string) (i *Image) {
 	return
 }
 
-// loads the image at path. the image element will have the same size as the image.
+// loads the image at path. the image element will have the same size as the image. If no image is provided, or if the
+// provided image fails to load for whatever reason, size will be 1x1.
 func (i *Image) Init(pos vec.Coord, depth int, path string) {
-	i.image = gfx.ImportXPData(path)
-	i.Element.Init(i.image.Size(), pos, depth)
+	i.LoadImage(path)
+	if i.image.Ready() {
+		i.Element.Init(i.image.Size(), pos, depth)
+	} else {
+		i.Element.Init(vec.Dims{1, 1}, pos, depth)
+	}
 	i.TreeNode.Init(i)
+}
+
+// Loads an xp image from disk to be displayed in the image element.
+func (i *Image) LoadImage(path string) {
+	if path == "" {
+		return
+	}
+
+	i.image = gfx.ImportXPData(path)
+	if i.image.Ready() {
+		if i.image.Size() != i.Size() {
+			i.Resize(i.image.Size())
+		}
+		i.Updated = true
+	}
 }
 
 func (i *Image) Render() {
